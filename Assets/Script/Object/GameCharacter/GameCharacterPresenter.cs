3ed067f98f5aeafce54f47df75d0114a451b3436
YuBis <cshoon1996@gmail.com>
2024-10-04 10:40:40 +0900

    Update GameCharacterPresenter.cs

--- Assets/Script/Object/GameCharacter/GameCharacterPresenter.cs
+++ Assets/Script/Object/GameCharacter/GameCharacterPresenter.cs
@@ -49,34 +49,31 @@ public class GameCharacterPresenter : IMovable, IJumpable, IAttackable
 
     void _CheckGroundStatus()
     {
+        if(!Model.IsJumping)
+            return;
+
+        float verticalVelocity = View.RIGIDBODY.totalForce.y;
+        if (verticalVelocity > 0)
+            return;
+            
         float groundCheckDistance = 0.1f;
         LayerMask groundLayer = LayerMask.GetMask("Ground");
 
         RaycastHit2D hit = Physics2D.Raycast(View.TRANSFORM.position, Vector2.down, groundCheckDistance, groundLayer);
         bool isGrounded = hit.collider != null;
 
-        if (isGrounded && View.RIGIDBODY.velocity.y <= 0 && Model.IsJumping)
+        if (isGrounded)
             OnLand();
     }
 
-    bool _CheckWallCollision()
+    bool _CheckWallCollision(float direction)
     {
-        Collider2D collider = View.COLLIDER;
-        if (collider == null)
-            return false;
-
-        float colliderHalfWidth = collider.bounds.extents.x;
-        float extraDistance = 0.05f;
+        float colliderHalfWidth = View.COLLIDER.bounds.extents.x;
+        float extraDistance = 0.1f;
+        float checkDistance = colliderHalfWidth + extraDistance;
 
         LayerMask groundLayer = LayerMask.GetMask("Grounds");
-
-        Vector2 direction = GetRotation().y > 0 ? Vector2.right : Vector2.left;
-
-        Vector2 rayOrigin = (Vector2)View.TRANSFORM.position + direction * colliderHalfWidth;
-
-        RaycastHit2D hit = Physics2D.Raycast(rayOrigin, direction, extraDistance, groundLayer);
-
-        Debug.DrawRay(rayOrigin, direction * extraDistance, Color.red);
+        RaycastHit2D hit = Physics2D.Raycast(View.TRANSFORM.position, direction > 0 ? Vector2.right : Vector2.left, checkDistance, groundLayer);
 
         return hit.collider != null;
     }
@@ -84,7 +81,7 @@ public class GameCharacterPresenter : IMovable, IJumpable, IAttackable
     public void Move(float direction)
     {
         var force = direction * (float)Model.MoveSpeed * Time.deltaTime;
-        if (_CheckWallCollision())
+        if (_CheckWallCollision(direction))
             force = 0;
 
         View.ApplyMovement(force);
